250bc3df0cfd4f06f2d67880aabc27153bd5f2e6
Luiz Carlos <lewiscontato99@gmail.com>
2021-10-27 17:27:08 -0300

    added crud to the API

---
+++ Mimic Api/Mimic Api/Controllers/PalavrasController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimicApi.Database;
+using MimicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mimic_Api.Controllers
+{
+
+    [Route("api/palavras")] //nome api/mais depois o nome do controller Palavras
+
+    [ApiController]
+    public class PalavrasController:ControllerBase
+    {
+        private readonly MimicContext _banco;
+        public PalavrasController(MimicContext  banco)
+        {
+            _banco = banco;
+
+
+
+        }
+
+
+
+        //Para App, obter informações de todas palavras/
+
+        //no caso retorna todos // /api/palavras
+
+        //app -- /api/palavras
+        //app -- /api/palavras?numero
+        [HttpGet]
+        public ActionResult ObterTodas(DateTime? data,int? pagnumero)
+        {
+            var item = _banco.Palavras.AsQueryable();
+
+            if (data.HasValue)
+            {
+                item = item.Where(a => a.Criado > data.Value|| a.Atualizado > data.Value);
+
+            }
+
+            //retornando um ok vai retornar
+            // para o metodo mais popular no caso JSON
+            return  Ok(item);
+
+           
+
+
+        }
+
+        //metodos para Web,
+        //no usando Crud.
+
+         // /api/palavras/1 (1 e o id).
+        [HttpGet("{id}")]
+        public ActionResult Obter(int id)
+
+        {
+            var obj = _banco.Palavras.Find(id);
+            if (obj ==null)
+            {
+                return NotFound(); //notfound tem que adotar toda vez que não localizar informação
+                //ou pode retornar o status do codigo no caso
+                // return StatusCode(404);
+
+
+            }
+
+
+
+
+
+            return Ok(_banco.Palavras.Find(id)); //quando dar certo retorna ok que no caso e 200
+        }
+
+
+         //para cadastrar não precisa acessar nenhum id
+        //assim pode acessar igual o metodo para obter todos api/palavras
+        [HttpPost]
+        public ActionResult Cadastrar([FromBody]Palavra Palavra)
+        {
+            _banco.Palavras.Add(Palavra);
+            _banco.SaveChanges();
+            return Created($"/api/palavras/{Palavra.Id}",Palavra);
+            //return created e usado quando e feito o cadastro de intem, 
+            //ai o sistema vai redicionar o usario para o cadastro que mesmo acabou de fazer.
+            //pela url /api/palavras/id da palavra cadastra, e no corpo retorna a palavra.
+
+        }
+
+        // -- /api/palavras/1 (put:id,nome,ativo,pontução,criacao).
+
+       // /api/palavras/1 (1 e o id).
+        [HttpPut("{id}")]
+        public ActionResult Atualizar(int id,[FromBody]Palavra palavra)
+        {
+
+
+            //codigo antigo   var obj = _banco.Palavras.find(id);
+            var obj = _banco.Palavras.AsNoTracking().FirstOrDefault(banco => banco.Id == id); //se tiver problema usando o find usa o codigo
+            //_banco.Palavras.AsNoTracking().FirstOrdefault(); 
+            //porque o entity framework mapeia toda consulta e guarda, e como criamos outro var obj o entity framework informa que tem 2 objetos do mesmo tipo o var obj e o palavra.id abaixo que tentamos consutlar.
+            if (obj == null)
+            {
+                return NotFound(); //notfound tem que adotar toda vez que não localizar informação
+                //ou pode retornar o status do codigo no caso
+                // return StatusCode(404);
+
+
+            }
+
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+
+        // -- /api/palavras/1 (delete)
+
+       
+
+        [HttpDelete("{id}")]
+        public ActionResult  Deletar(int id)
+        {
+
+            var obj = _banco.Palavras.Find(id);
+            if (obj == null)
+            {
+                return NotFound(); //notfound tem que adotar toda vez que não localizar informação
+                //ou pode retornar o status do codigo no caso
+                // return StatusCode(404);
+
+
+            }
+
+            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            _banco.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Mimic Api/Mimic Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MimicApi.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mimic_Api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+           /* services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling =
+                  Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            }) //esse codigo vai ignorar a referncia sitrica na seriliza��o na resposta json no metodo Action.
+            ; */
+            services.AddControllers();
+
+            services.AddDbContext<MimicContext>(opt =>
+            {
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
+
+         
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseStatusCodePages(); //quando fazer um request, e retornar exemplo 404
+            //usando esse metodo vai retornar uma mensagem mais informal para o lado do cliente.
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
